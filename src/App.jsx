1a918e79ae2ea5b50361694e43322fd122e456a3
cortex05 <havinginitiative@gmail.com>
2024-02-07 07:44:36 -0600

    feat: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>Vite + React!!!S!</h1>
+    </>
+  )
+}
+
+export default App
